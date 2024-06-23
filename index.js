c3763198499d2509cc39860c1b4edf3ed110d8dd
ellio <elliot.markovich@gmail.com>
2024-06-23 15:22:02 -0400

    Added crawling of historical data spreadsheet

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const readXlsxFile = require('read-excel-file/node');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,21 +28,32 @@ const db = getFirestore();
 
 console.log("test");
 
-setDoc(doc(db, "data structures/write test document"), { test: "testing"});
-
-const keyword_array = ["string", "another string", "Jonathan", "play yard", "r space", "Minor"];
-
 const docRef = doc(db, "data structures", "keyword trie");
 const docSnap = await getDoc(docRef);
 const docSnapData = docSnap.data();
 
-for(var index = 0; index < keyword_array.length; index++){
-  addWordToTrie(keyword_array[index]);
-}
 
-console.log(docSnapData.root);
-
-setDoc(docRef, { root: docSnapData.root});
+// File path.
+readXlsxFile('class-averages.xlsx').then((rows) => {
+  // `rows` is an array of rows
+  // each row being an array of cells.
+  for (var i = rows.length - 2; i > 1; i--){
+    var course_code = rows[i][1];
+    if (course_code != rows[i+1][1]){
+      var new_course_code = course_code.substring(0, 4) + " " + course_code.substring(4, course_code.length);
+      var info_HTML = "<p>" + "Course: " + new_course_code + " (" + rows[i][5] + " credits)" + "</p>"  + "<p>" + "Historical averages:" + "</p>";
+      addWordToTrie(new_course_code);
+      while (course_code == rows[i][1]){
+        info_HTML += "<p>" + rows[i][2] + ": " + rows[i][3] + " (" + rows[i][4] + ")" + "</p>";
+        i--;
+      }
+      i++;
+      setDoc(doc(db, "keywords", new_course_code), { info: info_HTML });
+      console.log(info_HTML);
+    }
+  }
+  setDoc(docRef, { root: docSnapData.root});
+})
 
 function addWordToTrie(word){
   var buildNode = docSnapData.root;
